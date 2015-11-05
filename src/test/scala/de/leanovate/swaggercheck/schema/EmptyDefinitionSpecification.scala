d66ea59a0e9f502e43e3b44971c8ec22be1d5d02
Untoldwind <landru@untoldwind.net>
2015-11-05 22:36:36 +0100

    Improve coverage a bit

--- src/test/scala/de/leanovate/swaggercheck/schema/EmptyDefinitionSpecification.scala
+++ src/test/scala/de/leanovate/swaggercheck/schema/EmptyDefinitionSpecification.scala
@@ -1,5 +1,20 @@
 package de.leanovate.swaggercheck.schema
 
-class EmptyDefinitionSpecification {
+import com.fasterxml.jackson.databind.JsonNode
+import de.leanovate.swaggercheck.SwaggerChecks
+import org.scalacheck.{Gen, Properties}
+import org.scalacheck.Prop.forAll
 
+object EmptyDefinitionSpecification extends Properties("EmptyDefinition") {
+  val swaggerChecks = SwaggerChecks(SwaggerAPI(None, Map.empty, Map.empty))
+
+  property("generate") = forAll(EmptyDefinition.generate(swaggerChecks)) {
+    node: JsonNode =>
+      node.isNull
+  }
+
+  property("verify") = forAll(Gen.oneOf(SchemaObject.arbitraryObj(swaggerChecks), SchemaObject.arbitraryArray(swaggerChecks), SchemaObject.arbitraryValue)) {
+    node: JsonNode =>
+      EmptyDefinition.verify(swaggerChecks, Nil, node).isSuccess
+  }
 }
