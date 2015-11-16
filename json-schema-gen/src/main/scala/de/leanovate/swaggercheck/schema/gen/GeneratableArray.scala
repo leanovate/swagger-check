8c6b6637f5bca1fc295787f35229306c009ee694
Untoldwind <landru@untoldwind.net>
2015-11-16 09:16:20 +0100

    Fix stackoverflow

--- json-schema-gen/src/main/scala/de/leanovate/swaggercheck/schema/gen/GeneratableArray.scala
+++ json-schema-gen/src/main/scala/de/leanovate/swaggercheck/schema/gen/GeneratableArray.scala
@@ -13,13 +13,17 @@ case class GeneratableArray(
                           (implicit nodeAdapter: NodeAdapter[T]): ValidationResult = definition.validate(model, path, node)
 
   override def generate(schema: GeneratableSchema): Gen[CheckJsValue] = {
-    val itemGenerator = definition.items.map(_.generate(schema.childContext)).getOrElse(schema.arbitraryValue)
-
     val min = definition.minItems.getOrElse(0)
     val max = Math.min(min + schema.maxItems, definition.maxItems.getOrElse(min + schema.maxItems))
-    for {
-      size <- Gen.choose(min, max)
-      elements <- Gen.listOfN(size, itemGenerator)
-    } yield CheckJsArray(definition.minItems, elements)
+
+    if (max == 0)
+      CheckJsArray(definition.minItems, Seq.empty)
+    else {
+      val itemGenerator = definition.items.map(_.generate(schema.childContext)).getOrElse(schema.arbitraryValue)
+      for {
+        size <- Gen.choose(min, max)
+        elements <- Gen.listOfN(size, itemGenerator)
+      } yield CheckJsArray(definition.minItems, elements)
+    }
   }
 }
