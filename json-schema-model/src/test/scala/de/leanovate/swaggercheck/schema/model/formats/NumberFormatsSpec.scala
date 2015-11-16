a9b5b60cf401677fafe9226c10dd91d83f5635b1
Untoldwind <landru@untoldwind.net>
2015-11-16 20:38:42 +0100

    Add unit tests

---
+++ json-schema-model/src/test/scala/de/leanovate/swaggercheck/schema/model/formats/NumberFormatsSpec.scala
@@ -0,0 +1,24 @@
+package de.leanovate.swaggercheck.schema.model.formats
+
+import de.leanovate.swaggercheck.schema.model.JsonPath
+import org.scalatest.{MustMatchers, WordSpec}
+
+class NumberFormatsSpec extends WordSpec with MustMatchers {
+  "Float format" should {
+    "fail for numbers out of range" in {
+      NumberFormats.FloatNumber.validate(JsonPath(), BigDecimal.decimal(Float.MaxValue) + BigDecimal(1)).isSuccess mustBe false
+      NumberFormats.FloatNumber.validate(JsonPath(), BigDecimal.decimal(Float.MaxValue)).isSuccess mustBe true
+      NumberFormats.FloatNumber.validate(JsonPath(), BigDecimal.decimal(Float.MinValue) - BigDecimal(1)).isSuccess mustBe false
+      NumberFormats.FloatNumber.validate(JsonPath(), BigDecimal.decimal(Float.MinValue)).isSuccess mustBe true
+    }
+  }
+
+  "Double format" should {
+    "fail for numbers out of range" in {
+      NumberFormats.DoubleNumber.validate(JsonPath(), BigDecimal.decimal(Double.MaxValue) + BigDecimal(1)).isSuccess mustBe false
+      NumberFormats.DoubleNumber.validate(JsonPath(), BigDecimal.decimal(Double.MaxValue)).isSuccess mustBe true
+      NumberFormats.DoubleNumber.validate(JsonPath(), BigDecimal.decimal(Double.MinValue) - BigDecimal(1)).isSuccess mustBe false
+      NumberFormats.DoubleNumber.validate(JsonPath(), BigDecimal.decimal(Double.MinValue)).isSuccess mustBe true
+    }
+  }
+}
